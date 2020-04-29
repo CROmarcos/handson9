085f65f9e4b6b87f2f04dec7b7f5fdc3ad1b1631
CROmarcos <markopavlovic2010@hotmail.com>
2020-04-29 19:00:59 +0200

    added: files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {AuthProvider} from './context/AuthContext';
 import './App.css';
+import Articles from './components/Articles';
+import Article from './components/Article';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <AuthProvider><Articles/></AuthProvider>
+    );
+  };
 }
 
 export default App;
---
+++ src/components/Articles.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react';
+import Article from './Article';
+
+const Articles=(props)=>{
+    return(<>
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
+    </>);
+}
+
+export default Articles;
